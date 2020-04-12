76f530adeca72d5247eaa2841decb86862550f86
Nenad Panic <nenadpnc@gmail.com>
2020-04-12 18:18:53 +0200

    handle error on fetching place info

--- client/pages/place/index.jsx
+++ client/pages/place/index.jsx
@@ -5,21 +5,31 @@ import { getPlaceInfo } from '../../utils/dataAccess';
 function Place({ data }) {
   return (
     <div className="place-wrapper">
-      <div className="place-info">
-        <h2 className="place-title">{data.name}</h2>
-        <h3 className="place-info-address">{data.address}</h3>
-      </div>
-      <OpeningHours data={data.openingHours} />
+      {data.error ? (
+        <h3>Error fetching place information.</h3>
+      ) : (
+        <>
+          <div className="place-info">
+            <h2 className="place-title">{data.name}</h2>
+            <h3 className="place-info-address">{data.address}</h3>
+          </div>
+          <OpeningHours data={data.openingHours} />
+        </>
+      )}
     </div>
   )
 };
 
 Place.getInitialProps = async ({ query }) => {
-  // Fetch data from external API
-  const data = await getPlaceInfo(query.id);
-
-  // Pass data to the page via props
-  return { data }
+  try {
+    // Fetch data from external API
+    const data = await getPlaceInfo(query.id);
+  
+    // Pass data to the page via props
+    return { data }
+  } catch (err) {
+    return { data: { error: true } };
+  }
 };
 
 export default Place;
